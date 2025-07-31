ae951272ccdefaae7c13667998cbedda8fb4c60f
facupetitfour <facupetitfour@gmail.com>
2025-07-30 21:23:08 -0300

    2da modificacion al deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/MawysModernLanding/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
